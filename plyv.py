64361a8bed175f116aaba3c7ae7562059f30a523
aerong <570931413@qq.com>
2016-07-19 23:44:37 +0800

    plyv,py

---
+++ plyv.py
@@ -0,0 +1,9 @@
+﻿# Calculate the value of a one-dimensional polynominal at spot x
+# coef   : tuple,   arranged in descending power of x
+# num    : int,     represent the most significant order of the polynominal
+# x      : float,   independent variable
+def plyv(coef,num,x)
+    u=coef[-1]
+	for i in xrange(num-2,0,-1)
+	    u=u*x+coef[i]
+    return u
